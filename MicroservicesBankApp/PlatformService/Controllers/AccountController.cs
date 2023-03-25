c10768f9dd9b7b674e118a995bf18b32d19e8e44
viktorsbcs <electricjackiebeats@gmail.com>
2023-03-25 18:38:12 +0200

    account controller

---
+++ MicroservicesBankApp/PlatformService/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlatformService.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
