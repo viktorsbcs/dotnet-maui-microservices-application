f8fff2901ebaf555de82c55b786d55705a208a02
viktorsbcs <electricjackiebeats@gmail.com>
2023-03-25 18:54:43 +0200

    test commit

--- MicroservicesBankApp/PlatformService/Program.cs
+++ MicroservicesBankApp/PlatformService/Program.cs
@@ -35,6 +35,7 @@ namespace PlatformService
                 app.UseSwaggerUI();
             }
 
+
             app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
