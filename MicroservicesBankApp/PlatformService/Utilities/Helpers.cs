b83d487e8a8c5d75206da0dfd6a53193db3b1e94
viktorsbcs <electricjackiebeats@gmail.com>
2023-04-09 04:51:28 +0300

    Static Generate Random Id method adjustments

--- MicroservicesBankApp/PlatformService/Utilities/Helpers.cs
+++ MicroservicesBankApp/PlatformService/Utilities/Helpers.cs
@@ -2,7 +2,7 @@
 {
     internal static class Helpers
     {
-        internal static string GeneretaRandomUserId()
+        internal static string GeneretaRandomId()
         {
             return new Random().NextInt64(1000, 9999).ToString();
         }
